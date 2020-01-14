9ac66f7d0ec05be5f33884f6751cb30bf8cd5a36
Raul <raul_dark40@hotmail.com>
2020-01-14 17:49:35 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton funciona, ambas variables contienen la misma instancia.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton fallo, las variables contienen diferentes instancias.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Singleton
+    {
+        private Singleton() { }
+
+        private static Singleton _instance;
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+    }
+}
